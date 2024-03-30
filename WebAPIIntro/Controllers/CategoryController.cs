ae6565278a85f93efc337341d9b0e1a0e235c6a3
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2024-03-30 12:37:37 +0300

    category controller added

---
+++ WebAPIIntro/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIIntro.Models;
+
+namespace WebAPIIntro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            NorthwindContext db = new NorthwindContext();
+            List<Category> categories = db.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            NorthwindContext db = new NorthwindContext();
+            Category category = db.Categories.FirstOrDefault(x => x.CategoryId == id);
+
+            if(category == null)
+            {
+                return NotFound("Boyle bir category mevcut degil");
+            }
+            
+            return Ok(category);
+
+        }
+    }
+}
